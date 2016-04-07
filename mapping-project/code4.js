d391214180caa178a69f03b54645fc07f1f0fa83
Ted Dawson <dawson.ted@gmail.com>
2016-04-07 10:25:33 +0200

    Added files via upload

---
+++ mapping-project/code4.js
@@ -0,0 +1,13 @@
+//folgende Zeilen in den Code einfuegen um eine Linie zu zeichnen
+
+ { "type": "Feature",
+        "geometry": {
+          "type": "LineString",		  
+		  //xxx.xx, xxx.xx durch die Koordinaten fuer die Linie ersetzen
+          "coordinates": [
+            [xxx.xx, xxx.xx], [xxx.xx, xxx.xx], [xxx.xx, xxx.xx], [xxx.xx, xxx.xx]
+            ]
+          },
+        "properties": {
+          "prop0": "value0"}
+        },
